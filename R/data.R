725707bf42c9899e52e77becf193f34c72d2ddc8
Tomás Barcellos <tomasbarcellos@gmail.com>
2020-02-15 23:36:54 -0300

    inclui documentação das tabelas

--- R/data.R
+++ R/data.R
@@ -1,9 +1,13 @@
+#' Salários do ES
 #' @format mais de 6 milhoes de linahs e 3 colunas
+#' @docType data
 #' @keywords data
 #' @source RAIS/MTE
 "salarios"
 
+#' Vínculos do ES
 #' @format mais de 1 milhao de linhas e 11 colunas
+#' @docType data
 #' @keywords data
 #' @source RAIS/MTE
 "vinculos"
